ae0014f9ff8e9e662f1634ef6ea5e25e393e1681
배만진 <gentlemmj@gmail.com>
2024-01-16 22:50:14 +0900

    refactor: 암호화 복호화 로직을 객체로 분리(ResidentId -> Encryptor)

---
+++ src/main/java/com/o3/mj/domain/Encryptor.java
@@ -0,0 +1,46 @@
+package com.o3.mj.domain;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+public class Encryptor {
+    private static final String ENCRYPTION_ALGORITHM = "AES";
+    private static final SecretKey secretKey = generateKey();
+
+    public String encrypt(String originData) {
+        try {
+            Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] encryptedBytes = cipher.doFinal(originData.getBytes());
+
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error during encryption", e);
+        }
+    }
+
+    public String decrypt(String encrypted){
+        try {
+            Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encrypted));
+
+            return new String(decryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error during decryption", e);
+        }
+    }
+
+    private static SecretKey generateKey() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(ENCRYPTION_ALGORITHM);
+            keyGenerator.init(128);
+
+            return keyGenerator.generateKey();
+        } catch (Exception e) {
+            throw new RuntimeException("Error generating AES key", e);
+        }
+    }
+}
--- src/main/java/com/o3/mj/domain/ResidentId.java
+++ src/main/java/com/o3/mj/domain/ResidentId.java
@@ -1,57 +1,26 @@
 package com.o3.mj.domain;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.Transient;
 import lombok.NoArgsConstructor;
-
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import java.util.Base64;
-import java.util.Objects;
 
 @Embeddable
 @NoArgsConstructor
 public class ResidentId {
-    private static final String ENCRYPTION_ALGORITHM = "AES";
-    private static final SecretKey secretKey = generateKey();
+    @Transient
+    private final Encryptor encryptor = new Encryptor();
     private String encrypted;
 
+    public Encryptor getEncryptor() {
+        return encryptor;
+    }
+
+
     public ResidentId(String original) {
-        this.encrypted = encrypt(original);
+        this.encrypted = encryptor.encrypt(original);
     }
 
     public String toOriginal() {
-        try {
-            Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(this.encrypted));
-
-            return new String(decryptedBytes);
-        } catch (Exception e) {
-            throw new RuntimeException("Error during decryption", e);
-        }
-    }
-
-    private String encrypt(String data) {
-        try {
-            Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            byte[] encryptedBytes = cipher.doFinal(data.getBytes());
-
-            return Base64.getEncoder().encodeToString(encryptedBytes);
-        } catch (Exception e) {
-            throw new RuntimeException("Error during encryption", e);
-        }
-    }
-
-    private static SecretKey generateKey() {
-        try {
-            KeyGenerator keyGenerator = KeyGenerator.getInstance(ENCRYPTION_ALGORITHM);
-            keyGenerator.init(128);
-
-            return keyGenerator.generateKey();
-        } catch (Exception e) {
-            throw new RuntimeException("Error generating AES key", e);
-        }
+        return encryptor.decrypt(this.encrypted);
     }
 }

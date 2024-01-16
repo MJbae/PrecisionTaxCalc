2da732059e8d2b778a3eabef7020176d871a6e32
배만진 <gentlemmj@gmail.com>
2024-01-16 11:18:28 +0900

    init app project

---
+++ src/main/java/com/o3/mj/PrecisionTaxCalcApplication.java
@@ -0,0 +1,13 @@
+package com.o3.mj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecisionTaxCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecisionTaxCalcApplication.class, args);
+	}
+
+}

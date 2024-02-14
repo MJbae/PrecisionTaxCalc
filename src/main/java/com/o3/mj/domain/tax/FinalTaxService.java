d13661804ae8b5e92ee7db379601e5805ba78b05
배만진 <gentlemmj@gmail.com>
2024-02-14 14:56:41 +0900

    refactor: 결정세액 도메인 서비스 이름 간소화

--- src/main/java/com/o3/mj/domain/tax/FinalTaxService.java
+++ src/main/java/com/o3/mj/domain/tax/FinalTaxService.java
@@ -7,11 +7,11 @@ import java.math.RoundingMode;
 public class FinalTaxService {
     private static final BigDecimal LABOR_INCOME_TAX_RATE = BigDecimal.valueOf(0.55);
     private static final BigDecimal STANDARD_DEDUCTION_LIMIT = BigDecimal.valueOf(130000);
-    private final SpecialDeductionService specialDeductionServiceCalc = new SpecialDeductionService();
+    private final SpecialDeductionService specialDeductionService = new SpecialDeductionService();
 
     public BigDecimal calculate(Tax tax, BigDecimal retirementPensionDeduction) {
         BigDecimal laborIncomeDeduction = tax.getCalculatedTaxAmount().multiply(LABOR_INCOME_TAX_RATE);
-        BigDecimal specialDeduction = specialDeductionServiceCalc.calculate(tax);
+        BigDecimal specialDeduction = specialDeductionService.calculate(tax);
         BigDecimal standardDeduction = specialDeduction.compareTo(STANDARD_DEDUCTION_LIMIT) < 0
                 ? STANDARD_DEDUCTION_LIMIT : BigDecimal.ZERO;
 
